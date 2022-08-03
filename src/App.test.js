9f910585e133867b2bfba0dd12341fe375d10b54
Segjr <samuel8016@gmail.com>
2022-08-03 11:59:25 -0700

    corrected App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Enzyme, { shallow } from  'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import App from './App'
-import { render, screen } from '@testing-library/react';
+
 
 Enzyme.configure({adapter: new Adapter()})
 
